e91ad9dd7da36f577214a2b7a2d80b819327bbb1
zekefromnextweek <zekeknox59@gmail.com>
2019-08-22 14:44:11 -0700

    -added to db

--- Alternative Credit/scoreCreator.py
+++ Alternative Credit/scoreCreator.py
@@ -2,9 +2,12 @@ import tkinter
 import random
 import sqlite3
 window=tkinter.Tk()
+title=tkinter.Label(window,text='Welcome to the Alaternate Credit Score Generator')
+title.pack()
+conn = sqlite3.connect('D:\\Alternate-Credit-Score-\\Alt_Credit.db')
+c=conn.cursor()
 def practice():
-    conn = sqlite3.connect('D:\\Alternate-Credit-Score-\\Alt_Credit.db')
-    c=conn.cursor()
+
     c.execute('''select i_name, count(distinct b_OverdraftDate)
     from indvidual,payments,bankstatement
     where i_id= p_key and
@@ -13,5 +16,16 @@ def practice():
     ''')
     rows = c.fetchall()
     for row in rows:
-        print('ji')
+        str.configure(text=rows)
     conn.close()
+
+button=tkinter.Button(window,text='Display names',command=practice)
+button.pack()
+str=tkinter.Label(window)
+str.pack()
+window.mainloop()
+def calculateScore(randomId,paymentHistory,bankHistory):
+    ##This will calculate the credit Score
+    randomId=random.randint(1,6)
+    score=800
+    if():
